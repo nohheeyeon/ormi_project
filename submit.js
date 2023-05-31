65a217634fbc2178c84d46795b4c71998ec9d977
nohheeyeon <scarletberries@naver.com>
2023-05-31 17:11:34 +0900

    SNS 계정 추가

--- submit.js
+++ submit.js
@@ -5,118 +5,86 @@ const $chatList = document.querySelector("#chat-list");
 // openAI API
 let url = `https://estsoft-openai-api.jejucodingcamp.workers.dev/`;
 
-// checkbox 데이터를 담을 객체
-let checkboxData = {};
+// 사용자의 질문
+let question;
 
 // 질문과 답변 저장
 let data = [
-  {
-    role: "system",
-    content: "assistant는 친절한 답변가이다.",
-  },
+    {
+        role: "system",
+        content: "assistant는 친절한 답변가이다.",
+    },
 ];
+
+// 화면에 뿌려줄 데이터, 질문들
+let questionData = [];
 
 // input에 입력된 질문 받아오는 함수
 $input.addEventListener("input", (e) => {
-  questionData = e.target.value;
+    question = e.target.value;
 });
-
-// checkbox 데이터를 객체로 변환하는 함수
-const getCheckboxData = () => {
-  const $genderCheckboxes = document.querySelectorAll(".gender input[type='checkbox']");
-  const $ageCheckboxes = document.querySelectorAll(".age input[type='checkbox']");
-  const $placeCheckboxes = document.querySelectorAll(".place input[type='checkbox']");
-
-  checkboxData = {};
-
-  $genderCheckboxes.forEach((checkbox) => {
-    if (checkbox.checked) {
-      checkboxData["성별"] = checkbox.id;
-    }
-  });
-
-  $ageCheckboxes.forEach((checkbox) => {
-    if (checkbox.checked) {
-      checkboxData["나이"] = checkbox.id;
-    }
-  });
-
-  $placeCheckboxes.forEach((checkbox) => {
-    if (checkbox.checked) {
-      checkboxData["장소"] = checkbox.id;
-    }
-  });
-};
 
 // 사용자의 질문을 객체를 만들어서 push
 const sendQuestion = (question) => {
-  if (question) {
-    getCheckboxData();
-
-    const questionObject = {
-      role: "user",
-      content: question,
-      ...checkboxData,
-    };
-
-    data.push(questionObject);
-  }
+    if (question) {
+        data.push({
+            role: "user",
+            content: question,
+        });
+        questionData.push({
+            role: "user",
+            content: question,
+        });
+    }
 };
 
 // 화면에 질문 그려주는 함수
-const printQuestion = () => {
-  if (data.length > 0) {
-    let li = document.createElement("li");
-    li.classList.add("question");
-    li.innerText = data[data.length - 1].content;
-    $chatList.appendChild(li);
-  }
+const printQuestion = async () => {
+    if (question) {
+        let li = document.createElement("li");
+        li.classList.add("question");
+        questionData.forEach((el) => {
+            li.innerText = el.content;
+        });
+        $chatList.appendChild(li);
+        questionData = [];
+        question = false;
+    }
 };
 
 // 화면에 답변 그려주는 함수
 const printAnswer = (answer) => {
-  let li = document.createElement("li");
-  li.classList.add("answer");
-  li.innerText = answer;
-  $chatList.appendChild(li);
+    let li = document.createElement("li");
+    li.classList.add("answer");
+    li.innerText = answer.content;
+    $chatList.appendChild(li);
 };
+
 
 // api 요청보내는 함수
 const apiPost = async () => {
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-
-    if (!response.ok) {
-      throw new Error("API 요청에 실패하였습니다.");
-    }
-
-    const result = await response.json();
-
-    const answer = result.choices[0].message;
-    printAnswer(answer);
-
-    data.push({
-      role: "system",
-      content: answer,
-    });
-  } catch (error) {
-    console.log(error);
-  }
+    const result = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+        redirect: "follow",
+    })
+        .then((res) => res.json())
+        .then((res) => {
+            printAnswer(res.choices[0].message);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
 
 // submit
 $form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const question = $input.value;
-  $input.value = "";
-
-  sendQuestion(question);
-  printQuestion();
-  apiPost();
+    e.preventDefault();
+    $input.value = null;
+    sendQuestion(question);
+    apiPost();
+    printQuestion();
 });
