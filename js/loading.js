5d90c714171d62c24b005546b9d75c981def7f14
nohheeyeon <scarletberries@naver.com>
2023-06-02 16:34:08 +0900

    html 정리

--- js/loading.js
+++ js/loading.js
@@ -11,6 +11,6 @@
       // 2초 후 페이지 전환
       setTimeout(() => {
           // 페이지 전환 코드
-          window.location.href = "main.html";
+          window.location.href = "../html/main.html";
       }, 2000); // 2초 (2000밀리초) 후에 페이지 전환 실행
   }
